4e84f1e41bba828c303504dd17653aa193017a21
ghexian <418474125@qq.com>
2022-04-08 01:10:39 +0800

    dx12框架代码

---
+++ source/core/d3dApp.h
@@ -0,0 +1,13 @@
+#include "d3dUtil.h"
+#include "GameTImer.h"
+
+// 链接所需的d3d12库
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+class D3DApp
+{
+protected:
+	D3DApp(HINSTANCE hInstance);
+};
